fad50bddcf7daa9003bdfd87a268204bd156668f
Sergio Benavides <sergiobv1@gmail.com>
2017-07-28 22:58:03 +0200

    perfil html

--- public/index.js
+++ public/index.js
@@ -8,9 +8,9 @@ $(document).ready(
 			data:{},
 			method:"post",
 			success:function(res,textstatus,xhr){
-				$("#fases").empty();
+				$("#caratulas").empty();
 				var fiel=inicioPerfil(res.im);
-				$("#fases").html(fiel);
+				$("#caratulas").html(fiel);
 			}
 		});
 
@@ -22,9 +22,9 @@ $(document).ready(
 			data:{},
 			method:"post",
 			success:function(res,textstatus,xhr){
-				$("#fases").empty();
+				$("#caratulas").empty();
 				var cel=especifico(res.im);
-				$("#fases").html(cel);
+				$("#caratulas").html(cel);
 			}
 		});
 	});
@@ -35,9 +35,18 @@ $(document).ready(
 			data:{},
 			method:"post",
 			success:function(res,textstatus,xhr){
-				$("#fases").empty();
+				$("#caratulas").empty();
+				var texto="<div id='actuales'>"
 				var cel=leyendo(res.im);
-				$("#fases").html(cel);
+				var seg=siguiendo(res.im);
+				var ju=Jugando(res.im);
+				var ret=reto();
+				texto+=cel;
+				texto+=seg;
+				texto+=ju;
+				texto+=ret;
+				texto+="</div>";
+				$("#caratulas").html(texto);
 			}
 		});
 	});
@@ -71,18 +80,97 @@ function especifico(imagen){
 function leyendo(imagen){
 	var texto="";
 	
-	texto+="<fieldset class='field'> "+"<legend>leyendo</legend> <div id='conProgreso'>";
-	texto+=imagen+"<div id='datos' class='bordes'>"+
+	texto+="<fieldset class='field'> "+"<legend>leyendo</legend> <div class='conProgreso'>";
+	texto+=imagen+"<div class='datos '>"+
 							"<label>titulo de libro</label>"+
 							"<label>Autor</label>"+
+							"<div id='progreso' >"+
+								"<label>x de x</label>"+
+								"<progress value='20' max='100'>"+
+							"</div>"+
+						"</div>";
+	texto+=imagen+"<div class=' datos'>"+
+							"<label>titulo de libro</label>"+
+							"<label>Autor</label>"+
+							"<div id='progreso' >"+
+								"<label>x de x</label>"+
+								"<progress value='20' max='100'>"+
+							"</div>"+
+						"</div>";
+	texto+="</div></fieldset>";
+
+	return texto;
+}
+
+function siguiendo(imagen){
+	var texto="";
+	
+	texto+="<fieldset class='field'> "+"<legend>viendo</legend> <div class='conProgreso'>";
+	texto+=imagen+"<div class='datos '>"+
+							"<label>titulo de libro</label>"+
+							"<label>Autor</label>"+
+							"<div id='progreso' >"+
+								"<label>x de x</label>"+
+								"<progress value='20' max='100'>"+
+							"</div>"+
+						"</div>";
+	texto+=imagen+"<div class=' datos'>"+
+							"<label>titulo de libro</label>"+
+							"<label>Autor</label>"+
+							"<div id='progreso' >"+
+								"<label>x de x</label>"+
+								"<progress value='20' max='100'>"+
+							"</div>"+
+						"</div>";
+	texto+="</div></fieldset>";
+
+	return texto;
+}
+
+function Jugando(imagen){
+	var texto="";
+	
+	texto+="<fieldset class='field'> "+"<legend>Jugando</legend> <div class='conProgreso'>";
+	texto+=imagen+"<div class='datos '>"+
+							"<label>titulo de libro</label>"+
+							"<label>Autor</label>"+
+						"</div>";
+	texto+=imagen+"<div class=' datos'>"+
+							"<label>titulo de libro</label>"+
+							"<label>Autor</label>"+
+						"</div>";
+	texto+="</div></fieldset>";
+
+	return texto;
+}
+
+function reto(){
+	var texto="";
+	
+	texto+="<fieldset class='field'> "+"<legend>reto</legend> <div class='conProgreso'  style='flex-wrap:wrap;'>";
+	texto+="<div class='datos' style='margin-bottom:5px;'>"+
+							"<label>Lecturas</label>"+
 							"<div id='progreso' class='bordes'>"+
 								"<label>x de x</label>"+
 								"<progress value='20' max='100'>"+
 							"</div>"+
 						"</div>";
-	texto+=imagen+"<div id='datos' class='bordes'>"+
-							"<label>titulo de libro</label>"+
-							"<label>Autor</label>"+
+	texto+="<div class='datos' style='margin-bottom:5px;'>"+
+							"<label>peliculas</label>"+
+							"<div id='progreso' class='bordes'>"+
+								"<label>x de x</label>"+
+								"<progress value='20' max='100'>"+
+							"</div>"+
+						"</div>";
+	texto+="<div class='datos' style='margin-bottom:5px;'>"+
+							"<label>series</label>"+
+							"<div id='progreso' class='bordes'>"+
+								"<label>x de x</label>"+
+								"<progress value='20' max='100'>"+
+							"</div>"+
+						"</div>";
+	texto+="<div class='datos' style='margin-bottom:5px;'>"+
+							"<label>videojuegos</label>"+
 							"<div id='progreso' class='bordes'>"+
 								"<label>x de x</label>"+
 								"<progress value='20' max='100'>"+
